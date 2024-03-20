7e7daed73049b82ccd72711e506dcc1a76d7d4e0
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2024-03-19 21:31:53 -0400

    checkpoint 2

--- app/src/main/java/com/example/randompet/MainActivity.kt
+++ app/src/main/java/com/example/randompet/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.randompet
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +20,23 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        getDogImageURL()
+    }
+    private fun getDogImageURL() {
+        val client = AsyncHttpClient()
+        client["https://dog.ceo/api/breeds/image/random", object : JsonHttpResponseHandler() {
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
+                Log.d("Dog", "response successful")
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                errorResponse: String,
+                throwable: Throwable?
+            ) {
+                Log.d("Dog Error", errorResponse)
+            }
+        }]
     }
 }
